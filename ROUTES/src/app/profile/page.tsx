0dc83d4a6faa3aee1df037b24765ba2a71a74f90
NihalAmin <nihalamin16@gmail.com>
2025-03-05 20:23:36 +0530

    day 3

--- ROUTES/src/app/profile/page.tsx
+++ ROUTES/src/app/profile/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function profile() {
   return (
-    <div><h1>profile</h1></div>
+    <div>
+      <h1>profile</h1>
+    </div>
   )
 }
--- router-handler-demo/src/app/comments/route.ts
+++ router-handler-demo/src/app/comments/route.ts
@@ -1,7 +1,13 @@
 import {comments} from "./data";
+import { NextRequest } from "next/server";
 
-export async function GET(){
-    return Response.json(comments);
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+  return Response.json(filteredComments);
 }
 
 export async function POST(request: Request) {
@@ -12,4 +18,29 @@ export async function POST(request: Request) {
       headers: { "Content-Type": "application/json" },
       status: 201,
     });
+  } 
+
+  export async function PATCH(
+    request: Request,
+    { params }: { params: Promise<{ id: string }> }
+  ) {
+    const { id } = await params;
+    const body = await request.json();
+    const { text } = body;
+  
+    const index = comments.findIndex((comment) => comment.id === parseInt(id));
+    comments[index].text = text;
+    return Response.json(comments[index]);
   }
+  
+  export async function DELETE(
+    _request: Request,
+    { params }: { params: Promise<{ id: string }> }
+  ) {
+    const { id } = await params;
+    const index = comments.findIndex((comment) => comment.id === parseInt(id));
+    const deletedComment = comments[index];
+    comments.splice(index, 1);
+    return Response.json(deletedComment);
+  }
+  
