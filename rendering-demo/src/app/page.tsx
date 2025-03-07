c40c8989f1d3f31f0e2096e38ab295736b81c12d
NihalAmin <nihalamin16@gmail.com>
2025-03-07 11:40:24 +0530

    Update page.tsx

--- rendering-demo/src/app/page.tsx
+++ rendering-demo/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <Link href="/dashboard"> dashboard</Link>
+        <Link href="/dashboard"> dashboard </Link>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
